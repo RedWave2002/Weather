859cad999ee79d0b4c4a573fa5e60050a407d297
outoffram <89171981+outoffram@users.noreply.github.com>
2021-12-11 11:59:04 +0800

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -26,10 +26,17 @@ def main():
 
 
 def get_data(url):
-    head = {
-        "User-Agent": "Mozilla/5.0 = Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"
-    }
-    req = urllib.request.Request(url, headers = head)
+     user_agent_list = ["Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
+                    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+                    "Mozilla/5.0 (Windows NT 10.0; WOW64) Gecko/20100101 Firefox/61.0",
+                    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36",
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
+                    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36",
+                    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
+                    "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15",
+                    ]
+    headers = {'User-Agent': random.choice(user_agent_list)}
+    req = urllib.request.Request(url, headers = headers, timeout = 5)
     html = ""
     try:
         res = urllib.request.urlopen(req)
@@ -59,7 +66,7 @@ def send_msg(msg1, msg2, qq):
     url = 'https://qmsg.zendee.cn/group/' + KEY    # 群消息推送接口
     # url2 = 'https://qmsg.zendee.cn/send/' + KEY  # 私聊消息推送接口
 
-    response = requests.post(url, data = data)
+    response = requests.post(url, data = data, timeout = 5)
     # response = requests.post(url2, data = data)
 
 def manage_str(str):
